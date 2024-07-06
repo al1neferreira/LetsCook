272ae75d972b4736ef7e38fde28749f468c0e195
Aline Ferreira <alinfst@gmail.com>
2024-07-06 10:30:34 -0300

    update Constants (insert apikey)

---
+++ app/src/main/java/com/example/letscook/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.letscook.util
+
+import com.example.letscook.BuildConfig
+
+object Constants {
+
+    const val BASE_URL = "https://api.spoonacular.com/"
+    const val API_KEY = BuildConfig.apiKey
+}
